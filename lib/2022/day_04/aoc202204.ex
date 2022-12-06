aa5fa8e2382a96add1734c98b96fe2d07ec246e7
Victor Poiesz <victorpoiesz@google.com>
2022-12-05 17:43:53 -0800

    refactor

--- lib/2022/day_04/aoc202204.ex
+++ lib/2022/day_04/aoc202204.ex
@@ -3,37 +3,42 @@ defmodule AOC2022.Day04 do
   def parse(path) do
     File.stream!(path)
     |> Stream.map(&String.trim/1)
-    |> Stream.map(&String.split(&1, ",", trim: true))
-    |> Stream.map(&to_pair/1)
+    |> Stream.map(&parse_pair/1)
   end
 
-  def to_pair(arr) do
-    arr
-    |> Enum.map(&String.split(&1, "-", trim: true))
-    |> Enum.map(fn arr -> Enum.map(arr, &String.to_integer/1) end)
-    |> Enum.map(&apply(Range, :new, &1))
+  def parse_pair(line) do
+    line
+    |> String.split(",", trim: true)
+    |> then(fn [left, right] -> {parse_elf(left), parse_elf(right)} end)
   end
 
-  def is_fully_contains([a,b]) do
-    (a.first <= b.first) && (a.last >= b.last)
-    ||
-    (b.first <= a.first) && (b.last >= a.last)
+  def parse_elf(str) do
+    str
+    |> String.split("-")
+    |> then(fn [first, last] -> String.to_integer(first)..String.to_integer(last) end)
   end
 
-  def is_overlap([a,b]) do
-    !Range.disjoint?(a,b)
+  def is_fully_contains({first1..last1, first2..last2}) do
+    first1 >= first2 and last1 <= last2 or
+    first2 >= first1 and last2 <= last1
+  end
+
+  def is_overlap({a,b}) do
+    not Range.disjoint?(a,b)
+  end
+
+  def solve(path, filter) do
+    parse(path)
+    |> Enum.map(filter)
+    |> Enum.count(& &1)
   end
 
   def p1(path) do
-    parse(path)
-    |> Enum.map(&is_fully_contains/1)
-    |> Enum.count(& &1)
+    solve(path, &is_fully_contains/1)
   end
 
   def p2(path) do
-    parse(path)
-    |> Enum.map(&is_overlap/1)
-    |> Enum.count(& &1)
+    solve(path, &is_overlap/1)
   end
 
 end
--- test/2022/day_04_test.exs
+++ test/2022/day_04_test.exs
@@ -22,21 +22,21 @@ defmodule Day03Test do
   #end
 
   test "is_fully_contains" do
-    assert !is_fully_contains([2..4, 6..8])
-    assert !is_fully_contains([2..3, 4..5])
-    assert !is_fully_contains([5..7, 7..9])
-    assert is_fully_contains([2..8, 3..7])
-    assert is_fully_contains([6..6, 4..6])
-    assert !is_fully_contains([2..6, 4..8])
+    assert !is_fully_contains({2..4, 6..8})
+    assert !is_fully_contains({2..3, 4..5})
+    assert !is_fully_contains({5..7, 7..9})
+    assert is_fully_contains({2..8, 3..7})
+    assert is_fully_contains({6..6, 4..6})
+    assert !is_fully_contains({2..6, 4..8})
   end
 
   test "is_overlap" do
-    assert !is_overlap([2..4, 6..8])
-    assert !is_overlap([2..3, 4..5])
-    assert is_overlap([5..7, 7..9])
-    assert is_overlap([2..8, 3..7])
-    assert is_overlap([6..6, 4..6])
-    assert is_overlap([2..6, 4..8])
+    assert !is_overlap({2..4, 6..8})
+    assert !is_overlap({2..3, 4..5})
+    assert is_overlap({5..7, 7..9})
+    assert is_overlap({2..8, 3..7})
+    assert is_overlap({6..6, 4..6})
+    assert is_overlap({2..6, 4..8})
   end
 
   test "p1 - example", %{example_path: example_path} do
