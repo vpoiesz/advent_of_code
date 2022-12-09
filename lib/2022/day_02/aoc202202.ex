775732af605ef3064eb52e504f7dd3167edabd6b
Victor Poiesz <victorpoiesz@google.com>
2022-12-08 19:38:56 -0800

    stub fix

---
+++ lib/2022/day_02/aoc202202.ex
@@ -0,0 +1,17 @@
+defmodule AOC2022.Day02 do
+
+  #def parse(path) do
+  #  File.stream!(path)
+  #  |> Stream.map(&String.trim/1)
+  #end
+#
+  #def p1(path) do
+#
+  #end
+#
+  #def p2(path) do
+  #  parse(path)
+#
+  #end
+
+end
